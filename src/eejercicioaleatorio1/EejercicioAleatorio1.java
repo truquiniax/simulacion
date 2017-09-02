de2d06c399eaaa9f47b453ab7b85a6efd6e43f18
Andres <Andres@Andres-PC>
2017-09-02 18:59:09 -0500

    Presentado por Andres Trujillo y Diego Leon

---
+++ src/eejercicioaleatorio1/EejercicioAleatorio1.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eejercicioaleatorio1;
+
+
+
+
+
+public class EejercicioAleatorio1 {
+
+
+
+
+    public static void main(String[] args) {
+        Grafico i= new Grafico();
+        i.setLocationRelativeTo(null);
+        i.setVisible(true);
+/*
+        System.out.println("Introduzca la semilla a calcular: ");
+        c=leer.nextInt();
+        
+        X2.add(2.0);
+        
+        //2double []G = new double[10];    
+                
+        X2.add(c);
+        while(X2.size()<10){
+            X2.add((X2.get(n)+2)/(X2.get(n)*X2.get(n)));
+            n++;
+            U2.add(X2.get(n)/33);
+        }        
+            System.out.println("Numeros aleatorios T(x)"+X2);            
+            System.out.println("Numeros aleatorios G(x)"+U2);*/
+            //JOptionPane.showMessageDialog(null,"Numeros aleatorios G(x) \n"+U2);
+    
+        // TODO code application logic here
+    }
+
+    
+
+}
